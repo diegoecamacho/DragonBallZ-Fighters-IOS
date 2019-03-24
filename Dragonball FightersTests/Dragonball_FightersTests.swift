690850f1f2e635653f6eaf431bf021573bdc5fc3
Diego Camacho <DiegoDC97@gmail.com>
2019-03-24 13:09:25 -0400

    Initial Commit

---
+++ Dragonball FightersTests/Dragonball_FightersTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dragonball_FightersTests.swift
+//  Dragonball FightersTests
+//
+//  Created by Diego Camacho on 2019-03-24.
+//  Copyright © 2019 Diego Camacho. All rights reserved.
+//
+
+import XCTest
+@testable import Dragonball_Fighters
+
+class Dragonball_FightersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
